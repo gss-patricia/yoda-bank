15806c8d09e769492512a4228ac607e8f3e49b00
jemisonsantos <jemison.santoos@gmail.com>
2020-10-19 19:26:37 -0300

    ✨feature: Adicionado componente Register

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -1,11 +1,130 @@
-import React from 'react'
+import React from 'react';
+import {Button, CssBaseline, TextField, Paper, Grid, Typography,} from '@material-ui/core/';
+import {Link} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Logo from '../../assets/logo.svg';
+import background from '../../assets/background.svg'
 
-const Register = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {   
+    backgroundImage: `url(${background})`,  
+    opacity: 0.5,
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: theme.palette.primary.main,      
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  link:{
+    justifyContent: 'center'    
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },  
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },  
+  teste: {
+    opacity: 1,
+    color: 'while',
+    justifyContent: 'center' 
+  },  
+}));
+
+export default function SignInSide() {
+  const classes = useStyles();
+
   return (
-    <div>
-      Register Test      
-    </div>
-  )
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />      
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>          
+          <img src={Logo} alt="Logo"/>
+          <Typography component="h1" variant="h5">
+            Você para a força, deve entrar
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="nome"
+              label="Nome"
+              name="nome" 
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"              
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="cpfCNPJ"
+              label="CPF/CNPJ"
+              name="cpfCNPJ" 
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="senha"
+              label="Senha"
+              type="password"
+              id="senha"
+              autoComplete="current-password"
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="confirmarSenha"
+              label="Confirmar Senha"
+              type="password"
+              id="confirmarSenha"            
+            />            
+            <Button
+              type="submit"
+              fullWidth              
+              variant="contained"
+              color="primary"
+              className={`${classes.submit}`}
+            >
+              CADASTRAR
+            </Button>            
+            <Grid container className={classes.link}>              
+              <Grid item>
+                <Link to={'/login'}>
+                  {"Já sou da força"}
+                </Link>
+              </Grid>
+            </Grid>            
+          </form>
+        </div>
+      </Grid> 
+      <Grid item xs={false} sm={4} md={7} className={`${classes.image}`}>  
+        <p className={classes.teste}>Faça ou não faça,
+           tentativa não há!</p>
+      </Grid>
+    </Grid>
+  );
 }
-
-export default Register;
