977f438f6fd1f2cfb298de2a3ea4b67a4ae867ab
Ana Carolina <anac.ac.social@gmail.com>
2020-10-20 08:51:26 -0300

    launch page

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import {createMuiTheme, MuiThemeProvider} from "@material-ui/core"
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Theme from "./Theme";
 import PageRegister from "./pages/PageRegister";
+import PageLaunch from "./pages/PageLaunch";
 
 const App = () => (
   <BrowserRouter>  
     <Theme>
       <Switch>
         <Route path="/register" component={PageRegister} />                
+        <Route path="/launch" component={PageLaunch} />                
       </Switch>    
     </Theme>  
   </BrowserRouter>
---
+++ src/pages/PageLaunch.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Launch from '../components/launch/Launch';
+
+const PageLaunch = () => {
+  return(
+    <Launch />
+  )
+};
+
+export default PageLaunch;
