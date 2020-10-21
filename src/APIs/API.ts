dfcf745cee147f1b7493b156401b6c2263c8576a
jemisonsantos <jemison.santoos@gmail.com>
2020-10-20 21:12:28 -0300

    Adicionado URL publiscada do backend

--- src/APIs/API.ts
+++ src/APIs/API.ts
@@ -1,9 +1,9 @@
-export const baseURL = "http://localhost:8080"; //TODO: Atualizar a baseURL após publicação da api
+export const baseURL = "https://beertech-banco-lupulo.herokuapp.com";
 import IUser from "../Interfaces/IUser";
 
 export const USER_CREATE = (body: IUser) => {
   return {
-    url: baseURL + "/register",
+    url: baseURL + "/conta",
     options: {
       method: "POST",
       headers: {
