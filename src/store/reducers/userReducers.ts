fa3f11b19a1cfd8fd363b4221f4a7cbd67c64f69
jemisonsantos <jemison.santoos@gmail.com>
2020-10-22 20:05:47 -0300

    ✨feature: adicionado user Reducer

--- src/store/reducers/userReducers.ts
+++ src/store/reducers/userReducers.ts
@@ -1,6 +1,28 @@
 import { AnyAction } from "redux";
 import UserActions from "../actions/UserActions";
 
-const { CREATE } = UserActions;
-const userReducers: any = 0; //TODO: Implementar os reducers
+const initialState = {
+  user: "",
+  email: "",
+  profile: "",
+};
+
+export interface UserState {
+  user: string;
+  email: string;
+  profile: string;
+}
+
+const userReducers = (state: UserState = initialState, action: AnyAction) => {
+  switch (action.type) {
+    case UserActions.GET_USER:
+      return state;
+    case UserActions.SET_USER:
+      state = { ...state, ...action.payload.user };
+      return state;
+    default:
+      break;
+  }
+  return state;
+};
 export default userReducers;
