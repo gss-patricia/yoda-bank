5d775ff8b2c8a48de9e8b1d6ac2c0b02c3c38785
jemisonsantos <jemison.santoos@gmail.com>
2020-10-24 09:49:02 -0300

    fix merged

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
@@ -10,19 +9,6 @@ import PageRegister from "./pages/PageRegister";
 import PageDeposit from "./pages/PageDeposit";
 import PageHome from "./pages/PageHome";
 import PageLogin from "./pages/PageLogin";
-=======
-import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import RoutesPrivate from './routes/PrivateRoute';
-import store from './store';
-import Theme from './Theme';
-import LocalStorageActions from './store/actions/LocalStorageActions';
-import PageRegister from './pages/PageRegister';
-import PageDeposit from './pages/PageDeposit';
-import PageHome from './pages/PageHome';
-import PageLogin from './pages/PageLogin';
->>>>>>> 46ed5b324059a9139e415c80c87784697bfbd27c
 
 store.dispatch({
   type: LocalStorageActions.LOAD_LOCAL_STORAGE,
